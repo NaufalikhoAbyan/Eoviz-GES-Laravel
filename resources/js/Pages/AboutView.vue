e67f1305d049f5e3365e0b1533f02a4341cf41c8
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-05 16:32:43 +0700

    set up VILT Stack

---
+++ resources/js/Pages/AboutView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <p class="font-bold">About</p>
+    <Link :href="route('index')">Home</Link>
+</template>
---
+++ resources/js/Pages/IndexView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <p class="font-bold">Home</p>
+    <Link :href="route('about')">About</Link>
+</template>
