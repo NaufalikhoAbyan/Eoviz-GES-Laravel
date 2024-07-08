add2959396b1ec942abfe882988333c7b61b9325
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-08 14:01:49 +0700

    feat: Add Employee Type Model, Migration, Factory, and Seeder

---
+++ database/seeders/EmployeeTypeSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EmployeeType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployeeTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        EmployeeType::factory(5)->create();
+    }
+}
