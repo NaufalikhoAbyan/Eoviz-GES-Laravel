1fed005b67c09b84b633bf2cd1031e6539e4d016
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-10 09:40:18 +0700

    feat: Add show data component; Add ShowView for Employee Types

---
+++ resources/js/Pages/References/EmployeeType/ShowView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import PageTitle from "@/Pages/Components/PageTitle.vue";
+import DetailsData from "@/Pages/Components/Details/DetailsData.vue";
+
+const props = defineProps({
+    employeeType: Object
+})
+</script>
+
+<template>
+    <div class="flex justify-center">
+        <div class="card px-8 w-1/2">
+            <div class="flex justify-between items-center">
+                <PageTitle page-name="Employee Type" description="Show an employee type data"/>
+                <Link :href="route('employee-types.index')"><button class="primary-btn h-fit">Back</button></Link>
+            </div>
+            <DetailsData title="Name" :value="props.employeeType.name"/>
+            <DetailsData title="Is Permanent" :value="props.employeeType['is_permanent']" :boolean="true"/>
+            <DetailsData title="order" :value="props.employeeType.order"/>
+            <DetailsData title="status" :value="props.employeeType.status"/>
+        </div>
+        <div>
+        </div>
+    </div>
+</template>
