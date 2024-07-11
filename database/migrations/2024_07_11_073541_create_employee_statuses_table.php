7796e6cbc12c68756ec0c55f02ce395ae98693b4
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-11 14:43:00 +0700

    feat: Add EmployeeStatus migration

--- database/migrations/2024_07_11_073541_create_employee_statuses_table.php
+++ database/migrations/2024_07_11_073541_create_employee_statuses_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('employee_statuses', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->boolean('is_active');
+            $table->integer('order');
+            $table->enum('status', ['Active', 'Not Active']);
+            $table->string('description', 255);
             $table->timestamps();
         });
     }
