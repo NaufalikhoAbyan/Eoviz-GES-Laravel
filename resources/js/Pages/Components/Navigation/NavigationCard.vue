5572c934b18b60b450d0f6343d4693533168ed32
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-11 08:58:42 +0700

    style: Add on hover style to NavigationCard component

--- resources/js/Pages/Components/Navigation/NavigationCard.vue
+++ resources/js/Pages/Components/Navigation/NavigationCard.vue
@@ -12,7 +12,7 @@ const props = defineProps({
     <div class="size-fit">
         <Link :href="route(props.link)">
             <div class="flex flex-col items-center gap-2 size-fit">
-                <div class="card w-fit bg-gray-300 p-6">
+                <div class="card w-fit bg-gray-300 p-6 hover:scale-110">
                     <img :src="props.src" alt="" class="size-12"/>
                 </div>
                 <p class="text-xl font-semibold opacity-60">{{ props.title }}</p>
