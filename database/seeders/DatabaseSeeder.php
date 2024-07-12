3472b7cd043342bcccba1734e54f3bf7b0354831
PasaYB <langpasa69@gmail.com>
2024-07-12 09:12:49 +0700

    fix: Deleting ReligionSeeder and BloodTypeSeeder import class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Database\Seeders\ReligionSeeder;
-use Database\Seeders\BloodTypeSeeder;
 
 class DatabaseSeeder extends Seeder
 {
