b0e317f7cc2f4db887a6840330889cd93befc454
PasaYB <langpasa69@gmail.com>
2024-07-13 14:34:56 +0700

    feat: Add Mass Assignment for Supervisor

--- app/Models/Supervisor.php
+++ app/Models/Supervisor.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Supervisor extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'street',
+        'country',
+        'state',
+        'city',
+        'postal_code',
+        'phone',
+        'mobile',
+        'email',
+        'status',
+        'description'
+    ];
 }
