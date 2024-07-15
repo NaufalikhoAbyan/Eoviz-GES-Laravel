3faf01ae677c7c140118256c9fa77d7c100a6a9c
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-15 12:17:33 +0700

    feat: Add true and false option for DetailData component

--- resources/js/Pages/Components/Details/DetailsData.vue
+++ resources/js/Pages/Components/Details/DetailsData.vue
@@ -2,13 +2,15 @@
 const props = defineProps({
     title: String,
     value: String,
-    boolean: Boolean
+    boolean: Boolean,
+    trueValue: String,
+    falseValue: String,
 });
 </script>
 
 <template>
     <div class="flex items-center py-8 border-b">
         <p class="w-1/3">{{ props.title }}</p>
-        <p class="text-xl font-bold w-2/3">{{ props.boolean ? props.value ? "Yes" : "No" : props.value }}</p>
+        <p class="text-xl font-bold w-2/3">{{ props.boolean ? props.value ? props.trueValue : props.falseValue : props.value }}</p>
     </div>
 </template>
