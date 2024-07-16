0a2baabd8d161efec6474695e9c716d9fd9ab431
PasaYB <langpasa69@gmail.com>
2024-07-16 09:07:11 +0700

    feat: Add ShiftWorkHour migration

--- database/migrations/2024_07_16_014909_create_shift_work_hours_table.php
+++ database/migrations/2024_07_16_014909_create_shift_work_hours_table.php
@@ -13,6 +13,20 @@ return new class extends Migration
     {
         Schema::create('shift_work_hours', function (Blueprint $table) {
             $table->id();
+            $table->string('code', length: 255)->unique();
+            $table->string('name', length: 255);
+            $table->dateTime('begin_log');
+            $table->dateTime('end_log');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->integer('start_tolerance');
+            $table->integer('end_tolerance');
+            $table->dateTime('break_start');
+            $table->dateTime('break_end');
+            $table->enum('cross_day', ['Yes', 'No']);
+            $table->enum('overtime_type', ['Request', 'Automatic']);
+            $table->enum('status', ['Active', 'Not Active']);
+            $table->string('description', length: 255);
             $table->timestamps();
         });
     }
