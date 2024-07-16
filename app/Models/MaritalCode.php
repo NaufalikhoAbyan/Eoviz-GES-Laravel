d5d6d47a275dca422a902b615740501af18a5f00
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-16 08:22:21 +0700

    feat: Add MaritalCode model, migration, controller, factory, and seeder

---
+++ app/Models/MaritalCode.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MaritalCode extends Model
+{
+    use HasFactory;
+}
