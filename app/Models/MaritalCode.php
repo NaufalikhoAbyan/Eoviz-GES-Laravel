24d96086c731d7317e1a6568e7750e8a27c54083
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-16 08:36:47 +0700

    feat: Add mass asignment and define relationship for Marital Codes

--- app/Models/MaritalCode.php
+++ app/Models/MaritalCode.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class MaritalCode extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'status',
+        'description',
+        'marital_status_id',
+    ];
+
+    public function maritalStatus(): BelongsTo
+    {
+        return $this->belongsTo(MaritalStatus::class);
+    }
 }
