baf75b7cc5181657ce2afb959537898d2454a1ab
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-16 13:43:04 +0700

    feat: Add mass asignment and define relation for Functional Position Types

--- app/Models/FunctionalPositionType.php
+++ app/Models/FunctionalPositionType.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FunctionalPositionType extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'functional_position_type_id',
+        'code',
+        'name',
+        'order',
+        'status',
+        'description',
+    ];
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(FunctionalPositionType::class, 'functional_position_type_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(FunctionalPositionType::class);
+    }
 }
