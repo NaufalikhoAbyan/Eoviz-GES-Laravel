fa74e0489713108f05410db7a2bb5b237ea14a8c
osa <156874251+PasaYB@users.noreply.github.com>
2024-07-16 08:12:52 +0700

    style: Add Supervisor Icon for MainLayout (#17)
    Co-authored-by: PasaYB <langpasa69@gmail.com>

--- resources/js/Pages/References/IndexView.vue
+++ resources/js/Pages/References/IndexView.vue
@@ -4,13 +4,14 @@ import NavigationCard from "@/Pages/Components/Navigation/NavigationCard.vue";
 
 <template>
     <div class="flex gap-12">
-        <NavigationCard link="employee-types.index" src="/images/assets/menus/employee.png" title="Empoloyee Types"/>
-        <NavigationCard link="blood-types.index" src="/images/assets/menus/small-blood-type.png" title="Blood Types"/>
-        <NavigationCard link="religions.index" src="/images/assets/menus/small-relegion.png" title="Religions"/>
+        <NavigationCard link="supervisors.index" src="/images/assets/menus/small-supervisior.png" title="Supervisors"/>
+        <NavigationCard link="employee-types.index" src="/images/assets/menus/employee.png" title="Employee Types"/>
         <NavigationCard link="employee-statuses.index" src="/images/assets/menus/small-employee-status.png" title="Employee Statuses"/>
-        <NavigationCard link="marital-code-taxes.index" src="/images/assets/menus/small-marital-tax.png" title="Marital Code Taxes"/>
-        <NavigationCard link="work-hours.index" src="/images/assets/menus/small-work-hour.png" title="Work Hours"/>
-        <NavigationCard link="marital-statuses.index" src="/images/assets/menus/small-marital-status.png" title="Marital Statuses"/>
+        <NavigationCard link="religions.index" src="/images/assets/menus/small-relegion.png" title="Religions"/>
+        <NavigationCard link="blood-types.index" src="/images/assets/menus/small-blood-type.png" title="Blood Types"/>
         <NavigationCard link="grade-types.index" src="/images/assets/menus/small-grade-type.png" title="Grade Types"/>
+        <NavigationCard link="work-hours.index" src="/images/assets/menus/small-work-hour.png" title="Work Hours"/>
+        <NavigationCard link="marital-code-taxes.index" src="/images/assets/menus/small-marital-tax.png" title="Marital Code Taxes"/>
+        <NavigationCard link="marital-statuses.index" src="/images/assets/menus/small-marital-status.png" title="Marital Statuses"/>
     </div>
 </template>
