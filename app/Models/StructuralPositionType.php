251447aeef201be4242eda71372f7c7931ef0b3e
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-16 10:31:16 +0700

    feat: Add mass asignment and define relation for Structural Position Types

--- app/Models/StructuralPositionType.php
+++ app/Models/StructuralPositionType.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StructuralPositionType extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'structural_position_type_id',
+        'code',
+        'name',
+        'order',
+        'status',
+        'description',
+    ];
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(StructuralPositionType::class);
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(StructuralPositionType::class);
+    }
 }
