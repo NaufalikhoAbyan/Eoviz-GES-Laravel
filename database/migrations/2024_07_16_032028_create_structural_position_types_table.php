eb65848385bfc98d13a100921a5e33f4b5058b0e
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-16 10:29:02 +0700

    feat: Add migration for Structural Position Types

--- database/migrations/2024_07_16_032028_create_structural_position_types_table.php
+++ database/migrations/2024_07_16_032028_create_structural_position_types_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\StructuralPositionType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('structural_position_types', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(StructuralPositionType::class)->nullable()->constrained()->restrictOnDelete();
+            $table->string('code', 255);
+            $table->string('name', 255);
+            $table->integer('order')->unique();
+            $table->enum('status', ['Active', 'Not Active']);
+            $table->string('description', 255);
             $table->timestamps();
         });
     }
