0ef2d24515f8f4d76e9f41a76b3896aab2ed1b77
NaufalikhoAbyan <apradana448@gmail.com>
2024-07-16 08:31:58 +0700

    feat: Add migration for Martial Codes

--- database/migrations/2024_07_16_012113_create_marital_codes_table.php
+++ database/migrations/2024_07_16_012113_create_marital_codes_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\MaritalStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('marital_codes', function (Blueprint $table) {
             $table->id();
+            $table->string('code', 255);
+            $table->string('name', 255);
+            $table->enum('status', ['Active', 'Not Active']);
+            $table->string('description', 255);
+            $table->foreignIdFor(MaritalStatus::class)->constrained()->restrictOnDelete();
             $table->timestamps();
         });
     }
