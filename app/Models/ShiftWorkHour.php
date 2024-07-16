fa7533e16bdcb000bb3e88e54cbb32792580d131
PasaYB <langpasa69@gmail.com>
2024-07-16 09:12:12 +0700

    feat: Add Mass Assignment to ShiftWorkHour Model

--- app/Models/ShiftWorkHour.php
+++ app/Models/ShiftWorkHour.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class ShiftWorkHour extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'code',
+        'name',
+        'begin_log',
+        'end_log',
+        'start',
+        'end',
+        'start_tolerance',
+        'end_tolerance',
+        'break_start',
+        'break_end',
+        'cross_day',
+        'overtime_type',
+        'status',
+        'description'
+    ];
 }
