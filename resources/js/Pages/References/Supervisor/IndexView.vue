9e21bee13e7c94325bb016be6d214e76cb0d7a64
PasaYB <langpasa69@gmail.com>
2024-07-22 09:37:12 +0700

    fix: Remove some records

--- resources/js/Pages/References/Supervisor/IndexView.vue
+++ resources/js/Pages/References/Supervisor/IndexView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
-import { Country, State } from 'country-state-city';
 import PageTitle from '@/Pages/Components/PageTitle.vue';
 import ActionButtons from '@/Pages/Components/ActionButtons.vue';
 const props = defineProps({
@@ -24,12 +23,6 @@ const props = defineProps({
                         <th class="table-item">No.</th>
                         <th class="table-item">Name</th>
                         <th class="table-item">Street</th>
-                        <th class="table-item">Country</th>
-                        <th class="table-item">State</th>
-                        <th class="table-item">City</th>
-                        <th class="table-item">Postal Code</th>
-                        <th class="table-item">Phone</th>
-                        <th class="table-item">Mobile</th>
                         <th class="table-item">Email</th>
                         <th class="table-item">Status</th>
                         <th class="table-item">Action</th>
@@ -40,12 +33,6 @@ const props = defineProps({
                         <td class="table-item">{{ index + 1 }}</td>
                         <td class="table-item">{{ supervisor.name }}</td>
                         <td class="table-item">{{ supervisor.street }}</td>
-                        <td class="table-item">{{ Country.getCountryByCode(supervisor.country).name }}</td>
-                        <td class="table-item">{{ State.getStateByCodeAndCountry(supervisor.state, supervisor.country).name }}</td>
-                        <td class="table-item">{{ supervisor.city }}</td>
-                        <td class="table-item">{{ supervisor.postal_code }}</td>
-                        <td class="table-item">{{ supervisor.phone }}</td>
-                        <td class="table-item">{{ supervisor.mobile }}</td>
                         <td class="table-item">{{ supervisor.email }}</td>
                         <td class="table-item">{{ supervisor.status }}</td>
                         <td class="table-action-item">
